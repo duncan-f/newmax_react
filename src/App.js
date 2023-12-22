4d0c670e4dc55f804950463832e7553558e2e388
dunCan <f0uad.f@hotmail.com>
2023-12-22 11:48:38 +0300

    :white_check_mark: fetching data from given URL

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://www.wildberries.ru/catalog/146972802/detail.aspx`, {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setLoading(false);
+        setData(data);
+      })
+      .catch((e) => {
+        console.error(e);
+        setError(e);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        {loading && <h1>Loading...</h1>}
+        {!loading && <h1>{data}</h1>}
       </header>
+      {error && <p>{error.message}</p>}
     </div>
   );
 }
